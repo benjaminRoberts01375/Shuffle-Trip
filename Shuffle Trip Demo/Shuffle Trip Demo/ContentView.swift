d27834f88ab30d79f0760f5dfad6f65c8ad56834
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2022-11-29 01:33:55 -0500

    User location displayed on map

--- Shuffle Trip Demo/Shuffle Trip Demo/ContentView.swift
+++ Shuffle Trip Demo/Shuffle Trip Demo/ContentView.swift
@@ -11,11 +11,11 @@ import MapKit
 struct ContentView: View {
 
     @StateObject private var viewModel = ContentViewModel()
-    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 44.4731, longitude: -73.2041), span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
     
     var body: some View {
         VStack {
-            Map(coordinateRegion: $region, showsUserLocation: true)
+            Map(coordinateRegion: $viewModel.region, showsUserLocation: true)
+                .accentColor(Color(.systemBlue))
                 .ignoresSafeArea()
                 .onAppear {
                     viewModel.checkIfLocationServicesIsEnabled()
@@ -27,44 +27,5 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
-}
-
-final class ContentViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
-    var locationManager: CLLocationManager?
-    
-    func checkIfLocationServicesIsEnabled() {
-        if CLLocationManager.locationServicesEnabled() {
-            locationManager = CLLocationManager()
-            locationManager?.desiredAccuracy = kCLLocationAccuracyBest // Default option, but I'm like that :)
-            locationManager!.delegate = self
-            Alert(title: Text("Yay!"))
-        }
-        else {
-            Alert(title: Text("No good."))
-        }
-    }
-    
-    private func checkLocationAuthorization(){
-        guard let locationManager = locationManager else { return }
-        
-        switch locationManager.authorizationStatus {
-            
-        case .notDetermined:
-            locationManager.requestWhenInUseAuthorization()
-        case .restricted:
-            print("Location is restricted because of parental controls... I think")
-        case .denied:
-            print("Location is required, please enable in settings")
-        case .authorizedAlways, .authorizedWhenInUse:
-            break // Come back to this
-        @unknown default:
-            break
-        }
-    }
-    
-    // Called by delegate
-    public func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
-        checkLocationAuthorization()
     }
 }
