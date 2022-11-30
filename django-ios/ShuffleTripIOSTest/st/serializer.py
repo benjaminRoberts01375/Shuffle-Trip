d9b61fa190303b1d8f11b0244819a200476190b2
Joe-Marchesini <jmarchesini@outlook.com>
2022-11-29 23:22:44 -0500

    dev containers added and tested

--- django-ios/ShuffleTripIOSTest/st/serializer.py
+++ django-ios/ShuffleTripIOSTest/st/serializer.py
@@ -1,7 +1,8 @@
 from .models import Account
 from rest_framework import serializers
 
+
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
-        fields = ('name', 'type', 'created_at')
+        fields = ("name", "type", "created_at")
