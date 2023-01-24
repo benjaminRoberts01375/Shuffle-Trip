474e6f78e4e37b5d572c193ae93aa3327c3ef4b4
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-01-24 16:51:45 -0500

    Rename MainScreen to MainScreenMV

---
+++ Shuffle Trip/Shuffle Trip/MainScreenMV.swift
@@ -0,0 +1,16 @@
+// Jan 24, 2023
+// Ben Roberts
+
+import SwiftUI
+
+struct MainScreenMV: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainScreenMV_Previews: PreviewProvider {
+    static var previews: some View {
+        MainScreenMV()
+    }
+}
