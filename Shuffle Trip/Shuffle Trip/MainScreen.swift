f31eaed74fd64f09354882c8bdbc0da8bbb07d88
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-01-24 16:31:58 -0500

    Use "MainScreen" instead of "ContentView"

---
+++ Shuffle Trip/Shuffle Trip/MainScreen.swift
@@ -0,0 +1,20 @@
+//
+//  MainScreen.swift
+//  Shuffle Trip
+//
+//  Created by Ben Roberts on 1/24/23.
+//
+
+import SwiftUI
+
+struct MainScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        MainScreen()
+    }
+}
