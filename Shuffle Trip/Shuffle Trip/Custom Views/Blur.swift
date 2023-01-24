d6def263e4ecf271041b3caa60161817aa73ac68
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-01-24 16:31:22 -0500

    Add blur as separate file
    This is used for backgrounds

---
+++ Shuffle Trip/Shuffle Trip/Custom Views/Blur.swift
@@ -0,0 +1,16 @@
+// Jan 22, 2023
+// Ben Roberts
+
+import SwiftUI
+
+struct BlurView: UIViewRepresentable {
+    let style: UIBlurEffect.Style
+    
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        return UIVisualEffectView(effect: UIBlurEffect(style: style))
+    }
+    
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+        uiView.effect = UIBlurEffect(style: style)
+    }
+}
