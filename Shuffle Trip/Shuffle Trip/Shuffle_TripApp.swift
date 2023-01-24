379c8684f7ec7104d2214f5c781706dce92706f3
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-01-24 16:30:29 -0500

    Initialize new project

---
+++ Shuffle Trip/Shuffle Trip/Shuffle_TripApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shuffle_TripApp.swift
+//  Shuffle Trip
+//
+//  Created by Ben Roberts on 1/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Shuffle_TripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
