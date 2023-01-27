b48a4fe4fbfbccfa9d1e7f0822097cfd3c1a7c4b
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-01-26 20:16:14 -0500

    Better comments for BottomDrawer code

--- Shuffle Trip/Shuffle Trip/Custom Views/BottomDrawer.swift
+++ Shuffle Trip/Shuffle Trip/Custom Views/BottomDrawer.swift
@@ -24,46 +24,45 @@ struct BottomDrawer<Content: View>: View {
     var body: some View {
         GeometryReader { geometry in
             VStack {
-                VStack { // The drawer itself
-                    Capsule() // Grabber
-                        .fill(Color.secondary)
+                VStack {                                // The drawer itself
+                    Capsule()                           // Grabber
+                        .fill(Color.secondary)          // Dynamic color for dark/light mode
                         .opacity(0.5)
-                        .frame(width: 50, height: 5)
+                        .frame(width: 50, height: 5)    // Roughly the same size as most apps
                         .padding(10)
-                    content // Content passed in to show
-                    Spacer() // Shove all content to the top
+                    content                             // Content passed in to show
+                    Spacer()                            // Shove all content to the top
                 }
-                
-                .frame(width: geometry.size.width, height: geometry.size.height) // Fill the outer VStack
-                .background(BlurView(style: .systemUltraThinMaterial))
+                .frame(width: geometry.size.width, height: geometry.size.height)    // Fill the outer VStack
+                .background(BlurView(style: .systemUltraThinMaterial))              // Set frosted background
                 .cornerRadius(12)
                 .shadow(radius: 3)
-                .offset(y: geometry.size.height - offset)
-                .onChange(of: goFull) { value in
+                .offset(y: geometry.size.height - offset)                           // Lower offset = lower on screen
+                .onChange(of: goFull) { value in                                    // Is only called when goFull changes
                     withAnimation (.interactiveSpring(response: 0.25, dampingFraction: 0.75)) {
-                        if value {
+                        if value {                                                  // If goFull, then bounce up to max size
                             offsetCache = offset
                             offset = snapPoints.max() ?? 500
                             return
                         }
-                        offset = offsetCache
+                        offset = offsetCache                                        // Otherwise, restore to previous position
                     }
                 }
-                .gesture( // Drag controller
+                .gesture (                                                          // Drag controller
                     DragGesture()
                         .onChanged { value in
-                            offset -= value.translation.height - previousDrag // Inverted to allow for smaller values to be at bottom
-                            previousDrag = value.translation.height // Save current drag distance to allow for relative positioning on the line above
+                            offset -= value.translation.height - previousDrag       // Inverted to allow for smaller values to be at bottom
+                            previousDrag = value.translation.height                 // Save current drag distance to allow for relative positioning on the line above
                         }
                         .onEnded { value in
                             if snapPoints.count > 0 {
-                                withAnimation (.interactiveSpring(response: 0.2, dampingFraction: 1/2)) { // Bounce once
+                                withAnimation (.interactiveSpring(response: 0.2, dampingFraction: 1/2)) {
                                     if goFull {
                                         offset = snapPoints.max() ?? 500
                                     }
                                     else {
-                                        let distances = snapPoints.map{abs(offset - $0)} // Figure out how far away sheet is from provided heights
-                                        offset = snapPoints[distances.firstIndex(of: distances.min() ?? 500) ?? 0] // Find closest height and set it
+                                        let distances = snapPoints.map{abs(offset - $0)}                            // Figure out how far away sheet is from provided heights
+                                        offset = snapPoints[distances.firstIndex(of: distances.min() ?? 500) ?? 0]  // Find closest height and set it
                                     }
                                 }
                             }
