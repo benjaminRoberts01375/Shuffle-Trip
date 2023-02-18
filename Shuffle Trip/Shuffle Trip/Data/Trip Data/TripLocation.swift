46174686b1ed16eed143aca2122bc5199be19af4
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-02-18 02:10:25 -0500

    Initial implementation for reading trip JSON

--- Shuffle Trip/Shuffle Trip/Data/Trip Data/TripLocation.swift
+++ Shuffle Trip/Shuffle Trip/Data/Trip Data/TripLocation.swift
@@ -9,28 +9,137 @@ public class TripLocation {
     /// Location of the trip
     var coordinate: CLLocationCoordinate2D
     /// How far fro the location does teh trip span
-    var radius: CGFloat = MapDetails.defaultRadius
+    var radius: CGFloat
     /// Activities to be had at the trip
-    var activityLocations: [Activity] = [] // TODO: [2/20/2023] Call to server to generate activites
+    var activityLocations: [Businesses]
     /// User has selected this trip for editing/viewing
-    var isSelected: Bool = true
+    var isSelected: Bool
     /// An unique identifier for each trip
-    let id = UUID()
+    let id: UUID
     /// ID for polygon
-    var polyID = 0
+    var polyID: Int
     
     init(coordinate: CLLocationCoordinate2D) {
         self.coordinate = coordinate
+        self.radius = MapDetails.defaultRadius
+        self.activityLocations = []
+        self.isSelected = true
+        self.id = UUID()
+        self.polyID = 0
+        print("Starting...")
+        GenerateActivities()
     }
     
-    /// Something for the user to do during their trip
-    struct Activity {
-        /// Start date and time
-        let start: DateComponents
-        /// End date and time
-        let end: DateComponents
-        /// Where the activity is
-        let location: CLLocationCoordinate2D
+//    /// Something for the user to do during their trip
+//    struct Activity {
+//        /// Start date and time
+//        let start: DateComponents
+//        /// End date and time
+//        let end: DateComponents
+//        /// Where the activity is
+//        let location: CLLocationCoordinate2D
+//    }
+    
+    struct Businesses: Decodable {
+        let businesses: [Business]
+        let total: Int
+        let region: Region
+
+        enum CodingKeys: String, CodingKey {
+            case businesses
+            case total
+            case region
+        }
+    }
+
+    struct Business: Decodable {
+        let id: String
+        let alias: String
+        let name: String
+        let imageUrl: String
+        let isClosed: Bool
+        let url: String
+        let reviewCount: Int
+        let categories: [Category]
+        let rating: Int
+        let coordinates: Coordinates
+        let transactions: [String]
+        let location: Location
+        let phone: String
+        let displayPhone: String
+        let distance: Double
+
+        enum CodingKeys: String, CodingKey {
+            case id
+            case alias
+            case name
+            case imageUrl = "image_url"
+            case isClosed = "is_closed"
+            case url
+            case reviewCount = "review_count"
+            case categories
+            case rating
+            case coordinates
+            case transactions
+            case location
+            case phone
+            case displayPhone = "display_phone"
+            case distance
+        }
+    }
+
+    struct Category: Decodable {
+        let alias: String
+        let title: String
+    }
+
+    struct Coordinates: Decodable {
+        let latitude: Double
+        let longitude: Double
+    }
+
+    struct Location: Decodable {
+        let address1: String
+        let address2: String?
+        let address3: String?
+        let city: String
+        let zipCode: String
+        let country: String
+        let state: String
+        let displayAddress: [String]
+
+        enum CodingKeys: String, CodingKey {
+            case address1
+            case address2
+            case address3
+            case city
+            case zipCode = "zip_code"
+            case country
+            case state
+            case displayAddress = "display_address"
+        }
+    }
+
+    struct Region: Decodable {
+        let center: Center
+    }
+
+    struct Center: Decodable {
+        let longitude: Double
+        let latitude: Double
+    }
+
+    private func GenerateActivities() {
+        if let fileUrl = Bundle.main.url(forResource: "Trip", withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: fileUrl)
+                let decoder = JSONDecoder()
+                activityLocations = try decoder.decode([Businesses].self, from: data)
+                print(activityLocations)
+            } catch {
+                print("Error reading JSON file: \(error)")
+            }
+        }
     }
 }
 
