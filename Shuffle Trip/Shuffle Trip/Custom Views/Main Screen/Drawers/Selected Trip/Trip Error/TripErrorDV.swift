8db81449b5e7edb74a21265c72715f9efb83454c
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-03-22 13:09:36 -0400

    Moved error message to header and change styling

--- Shuffle Trip/Shuffle Trip/Custom Views/Main Screen/Drawers/Selected Trip/Trip Error/TripErrorDV.swift
+++ Shuffle Trip/Shuffle Trip/Custom Views/Main Screen/Drawers/Selected Trip/Trip Error/TripErrorDV.swift
@@ -11,38 +11,43 @@ struct TripErrorDV: DrawerView {
     }
     
     var header: some View {
-        HStack {
-            Spacer()
-            Button(action: {    // Trash button
-                controller.deleteTrip()
-            }, label: {
-                Image(systemName: "trash.circle.fill")
-                    .symbolRenderingMode(.hierarchical)
-                    .foregroundStyle(.red)
-                    .font(Font.title.weight(.bold))
-            })
-            Button(action: {    // Try again button
-                controller.reshuffleTrip()
-            }, label: {
-                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
-                    .symbolRenderingMode(.hierarchical)
-                    .foregroundStyle(.blue)
-                    .font(Font.title.weight(.bold))
-            })
+        VStack {
+            HStack {
+                Spacer()
+                Button(action: {    // Trash button
+                    controller.deleteTrip()
+                }, label: {
+                    Image(systemName: "trash.circle.fill")
+                        .symbolRenderingMode(.hierarchical)
+                        .foregroundStyle(.red)
+                        .font(Font.title.weight(.bold))
+                })
+                Button(action: {    // Try again button
+                    controller.reshuffleTrip()
+                }, label: {
+                    Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                        .symbolRenderingMode(.hierarchical)
+                        .foregroundStyle(.blue)
+                        .font(Font.title.weight(.bold))
+                })
+            }
+            HStack {
+                Image(systemName: "exclamationmark.triangle.fill")
+                    .padding(.horizontal, 1)
+                    .foregroundStyle(Color.red)
+                    .opacity(0.75)
+                Text("Failed to generate trip")
+                    .foregroundColor(.secondary)
+                    .font(.body)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .textCase(.uppercase)
+            }
+            .padding(.top, 8)
         }
     }
     
     var body: some View {
-        HStack {
-            Image(systemName: "exclamationmark.triangle.fill")
-                .padding(.horizontal, 1)
-                .foregroundStyle(Color.yellow)
-            Text("Failed to generate trip")
-                .foregroundColor(.secondary)
-                .font(.body)
-                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                .textCase(.uppercase)
-        }
+        EmptyView()
     }
 }
 
