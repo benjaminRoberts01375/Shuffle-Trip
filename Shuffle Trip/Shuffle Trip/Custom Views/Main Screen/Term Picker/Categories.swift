826c9852a23f6706614dfe57262a0685ce029972
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-03-22 19:59:23 -0400

    List all categories

--- Shuffle Trip/Shuffle Trip/Custom Views/Main Screen/Term Picker/Categories.swift
+++ Shuffle Trip/Shuffle Trip/Custom Views/Main Screen/Term Picker/Categories.swift
@@ -12,7 +12,16 @@ struct Categories: View {
                 ForEach(categories.categories.sorted(by: { (lhs, rhs) -> Bool in
                     lhs.category < rhs.category
                 }), id: \.category) { test in
-                    Text(test.category)
+                    Section {
+                        Text(test.category)
+                            .font(.title3.bold())
+                        
+                        ForEach(test.data.sorted(by: { (lhs, rhs) -> Bool in
+                            lhs < rhs
+                        }), id: \.self) { point in
+                            Text(point)
+                        }
+                    }
                 }
             }
         }
