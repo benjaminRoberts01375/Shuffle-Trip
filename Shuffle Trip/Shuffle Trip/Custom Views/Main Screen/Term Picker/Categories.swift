019edbc98b37bd71e3bf4f73eddcdd44cfa79f1f
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-03-23 02:10:42 -0400

    Added header

--- Shuffle Trip/Shuffle Trip/Custom Views/Main Screen/Term Picker/Categories.swift
+++ Shuffle Trip/Shuffle Trip/Custom Views/Main Screen/Term Picker/Categories.swift
@@ -7,52 +7,55 @@ struct Categories: View {
     @StateObject var topics: CategoryDataM
     
     var body: some View {
-        ScrollViewReader { proxy in
-            List {
-                ForEach(topics.topics.sorted(by: { (lhs, rhs) -> Bool in            // List all topics and sort alphabetically
-                    lhs.topic < rhs.topic
-                }), id: \.topic) { topic in
-                    Section {                                                       // Devide the topics
-                        Button(action: {
-                            if topics.CheckTopicSelection(topic: topic.topic) {
-                                topics.DeselectTopic(topic: topic.topic)
-                            }
-                            else {
-                                topics.SelectTopic(topic: topic.topic)
-                            }
-                        }, label: {
-                            HStack {
-                                Text(topic.topic)
-                                    .font(.title2.bold())
-                                    .foregroundColor(Color.primary)
-                                Image(systemName: topic.symbol)
-                                    .foregroundColor(topics.CheckTopicSelection(topic: topic.topic) ? .blue : .primary)
-                            }
-                        })
-                        .padding(.vertical, 5)
-                        
-                        ForEach(topic.categories.sorted(), id: \.self) { category in // List all categories and sort alphabetically
-                            HStack {
-                                Image(systemName: "checkmark")
-                                    .opacity(topics.CategoryIsSelected(topic: topic.topic, category: category) ? 1 : 0)
-                                    .foregroundColor(.blue)
-                                Button(action: {
-                                    if topics.CategoryIsSelected(topic: topic.topic, category: category) {
-                                        topics.DeselectCategory(topic: topic.topic, category: category)
-                                    }
-                                    else {
-                                        topics.SelectCategory(topic: topic.topic, category: category)
-                                    }
-                                }, label: {
-                                    Text(category)
+        NavigationStack {
+            ScrollViewReader { proxy in
+                List {
+                    ForEach(topics.topics.sorted(by: { (lhs, rhs) -> Bool in            // List all topics and sort alphabetically
+                        lhs.topic < rhs.topic
+                    }), id: \.topic) { topic in
+                        Section {                                                       // Devide the topics
+                            Button(action: {
+                                if topics.CheckTopicSelection(topic: topic.topic) {
+                                    topics.DeselectTopic(topic: topic.topic)
+                                }
+                                else {
+                                    topics.SelectTopic(topic: topic.topic)
+                                }
+                            }, label: {
+                                HStack {
+                                    Text(topic.topic)
+                                        .font(.title2.bold())
                                         .foregroundColor(Color.primary)
-                                })
+                                    Image(systemName: topic.symbol)
+                                        .foregroundColor(topics.CheckTopicSelection(topic: topic.topic) ? .blue : .primary)
+                                }
+                            })
+                            .padding(.vertical, 5)
+                            
+                            ForEach(topic.categories.sorted(), id: \.self) { category in // List all categories and sort alphabetically
+                                HStack {
+                                    Image(systemName: "checkmark")
+                                        .opacity(topics.CategoryIsSelected(topic: topic.topic, category: category) ? 1 : 0)
+                                        .foregroundColor(.blue)
+                                    Button(action: {
+                                        if topics.CategoryIsSelected(topic: topic.topic, category: category) {
+                                            topics.DeselectCategory(topic: topic.topic, category: category)
+                                        }
+                                        else {
+                                            topics.SelectCategory(topic: topic.topic, category: category)
+                                        }
+                                    }, label: {
+                                        Text(category)
+                                            .foregroundColor(Color.primary)
+                                    })
+                                }
                             }
                         }
                     }
                 }
+                .overlay(sectionIndexTitles(proxy: proxy))
             }
-            .overlay(sectionIndexTitles(proxy: proxy))
+            .navigationBarTitle("Categories")
         }
     }
     
