019212a0a30a2cdac74a9b306211c002bc61a1c8
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-03-27 03:46:43 -0400

    Setup base data

--- Shuffle Trip/Shuffle Trip/Data/API Handler.swift
+++ Shuffle Trip/Shuffle Trip/Data/API Handler.swift
@@ -4,4 +4,20 @@
 import SwiftUI
 
 struct APIHandler {
+    static let baseURL: String = "https://shuffle-trip-backend.herokuapp.com/"
+    
+    enum APIUrl: String {
+        case shuffleTrip = "filteredApi"
+        case friendDetauls = "getFriendsTrips"
+    }
+    
+    enum Errors: String, Error {
+        case decodeError = "Unable to decode data from the server."
+        case emptyResponse = "Got no data back from the server."
+        case encodeError = "Unable to encode data into a request."
+        case hostConnectionError = "Unable to connect to the server."
+        case sendPOSTError = "Unknown error."
+        case urlError = "Not able to generate server URL."
+    }
+    
 }
