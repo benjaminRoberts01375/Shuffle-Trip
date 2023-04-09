d19e7e652265c0ad8480a92edd4bbf4bdf0027d8
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-04-08 20:52:30 -0400

    Added documentation

--- Shuffle Trip/Custom Views/Main Screen/Drawers/Selected Trip/SelectedTripButtonsV.swift
+++ Shuffle Trip/Custom Views/Main Screen/Drawers/Selected Trip/SelectedTripButtonsV.swift
@@ -4,27 +4,41 @@
 import SwiftUI
 
 struct SelectedTripButtonsV: View {
+    /// Locations of each trip
     var tripLocations: TripLocations
-    @State var shuffleButtons: DisplayButtons
+    /// Determines which buttons to show when
+    @State var buttonState: DisplayButtons
+    /// Disallow the user to close the current drawer
     @State var disableCloseButton: Bool
-
+    
+    /// The initializer for the selected trip button manager
+    /// - Parameter tripLocations: Locations of each trip
     init(tripLocations: TripLocations) {
         self._disableCloseButton = State(initialValue: false)
-        self._shuffleButtons = State(initialValue: .normal)
+        self._buttonState = State(initialValue: .normal)
         self.tripLocations = tripLocations
     }
     
+    /// Available arrangements for button layouts
     enum DisplayButtons {
+        /// Confirmation to shuffle a trip
         case confirmShuffle
+        /// Buttons to show before user interaction
         case normal
     }
     
     var body: some View {
-        switch shuffleButtons {
-        case .confirmShuffle:
-            ConfirmShuffleButtonsV(tripLocations: tripLocations, buttonState: $shuffleButtons)
-        case .normal:
-            TripInteractionButtonsV(tripLocations: tripLocations, buttonState: $shuffleButtons)
+        switch buttonState {
+        case .confirmShuffle:                   // If showing the confirmation to shuffle an entire trip
+            ConfirmShuffleButtonsV(                 // Show the Confirm Shuffle Buttons view
+                tripLocations: tripLocations,
+                buttonState: $buttonState
+            )
+        case .normal:                           // If showing the normal buttons
+            TripInteractionButtonsV(                // Show the buttons for potential trip interaction
+                tripLocations: tripLocations,
+                buttonState: $buttonState
+            )
         }
     }
 }
