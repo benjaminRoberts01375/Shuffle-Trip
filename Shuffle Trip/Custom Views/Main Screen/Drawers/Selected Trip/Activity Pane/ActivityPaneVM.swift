3ee57b144c19ad49ec2805093ee767daa2f44081
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-04-08 21:12:34 -0400

    Added documentation

--- Shuffle Trip/Custom Views/Main Screen/Drawers/Selected Trip/Activity Pane/ActivityPaneVM.swift
+++ Shuffle Trip/Custom Views/Main Screen/Drawers/Selected Trip/Activity Pane/ActivityPaneVM.swift
@@ -4,9 +4,15 @@
 import SwiftUI
 
 final class ActivityPaneVM: ObservableObject {
+    /// An activity to display
     @Published var activity: Activity
+    /// Index of the activity within a  trip
     var index: Int
     
+    /// Init function for the Activity Pane view model
+    /// - Parameters:
+    ///   - activity: Activity to display
+    ///   - index: Index of the activity within a trip
     init(activity: Activity, index: Int) {
         self.activity = activity
         self.index = index
