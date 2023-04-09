11988d20e222ea4fb613a20cb61cd606ec96c430
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-04-08 20:56:48 -0400

    Added documentation

--- Shuffle Trip/Custom Views/Main Screen/Drawers/Selected Trip/Header Buttons/ConfirmShuffleButtonsVM.swift
+++ Shuffle Trip/Custom Views/Main Screen/Drawers/Selected Trip/Header Buttons/ConfirmShuffleButtonsVM.swift
@@ -4,9 +4,16 @@
 import SwiftUI
 
 final class ConfirmShuffleButtonsVM: ObservableObject {
+    /// Locations of each trip
     @Published var tripLocations: TripLocations
+    
+    /// Current buttons to show
     @Binding var buttonState: SelectedTripButtonsV.DisplayButtons
     
+    /// Initializer for ConfirmShuffleButtons's view model
+    /// - Parameters:
+    ///   - tripLocations: Locations of all the trips
+    ///   - buttonState: State of the buttons as a binding, passed in by a parent view
     init(tripLocations: TripLocations, buttonState: Binding<SelectedTripButtonsV.DisplayButtons>) {
         self.tripLocations = tripLocations
         self._buttonState = buttonState
--- Shuffle Trip/Custom Views/Main Screen/Drawers/Selected Trip/Header Buttons/TripInteractionButtonsVM.swift
+++ Shuffle Trip/Custom Views/Main Screen/Drawers/Selected Trip/Header Buttons/TripInteractionButtonsVM.swift
@@ -4,10 +4,17 @@
 import SwiftUI
 
 final class TripInteractionButtonsVM: ObservableObject {
+    /// Prevents the drawer from being closed
     @Published var disableCloseButton: Bool
+    /// Locations of each trip
     @Published var tripLocations: TripLocations
+    /// Current buttons to show
     @Binding var buttonState: SelectedTripButtonsV.DisplayButtons
     
+    /// Initializer for ConfirmShuffleButtons's view model
+    /// - Parameters:
+    ///   - tripLocations: Locations of all the trips
+    ///   - buttonState: State of the buttons as a binding, passed in by a parent view
     init(tripLocations: TripLocations, buttonState: Binding<SelectedTripButtonsV.DisplayButtons>) {
         self.disableCloseButton = false
         self.tripLocations = tripLocations
