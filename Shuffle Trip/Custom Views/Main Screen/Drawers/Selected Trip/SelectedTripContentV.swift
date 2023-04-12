436b8d5b6cf0dc6fa468b5646e3b8e3185082fe1
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-04-12 02:44:44 -0400

    Created skeleton of SelectedTripContentV

---
+++ Shuffle Trip/Custom Views/Main Screen/Drawers/Selected Trip/SelectedTripContentV.swift
@@ -0,0 +1,47 @@
+// Apr 12, 2023
+// Ben Roberts
+
+import SwiftUI
+
+struct SelectedTripContentV: View {
+    /// Locations of each trip
+    var tripLocations: TripLocations
+    /// Selected trip from trip locations
+    @State var selectedTrip: TripLocation!
+    /// Manages the content being shown in the drawer body
+    @State var contentType: ContentType
+    
+    /// The initializer for the selected trip content manager
+    /// - Parameter tripLocations: Locations of each trip
+    init(tripLocations: TripLocations) {
+        self.tripLocations = tripLocations
+        contentType = .info
+    }
+    
+    enum ContentType {
+        /// Display information about the trip
+        case info
+        /// Trip configuration
+        case settings
+    }
+    
+    /// Sets the cache for the selected trip based on what trip is found to be selected in TripLocations
+    private func setSelectedTrip() {
+        selectedTrip = tripLocations.tripLocations.first(where: { $0.isSelected }) ?? selectedTrip
+    }
+    
+    var body: some View {
+        switch contentType {
+        case .info:
+            EmptyView()
+        case .settings:
+            EmptyView()
+        }
+    }
+}
+
+struct SelectedTripContent_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectedTripContentV(tripLocations: TripLocations())
+    }
+}
