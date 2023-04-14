a6769804566d6e038116d34cc8a4a551d226a734
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-04-14 00:43:29 -0400

    Created a view model for the add activity button

---
+++ Shuffle Trip/Custom Views/Main Screen/Drawers/Selected Trip/Body Content/Add Activity Button/AddActivityButtonVM.swift
@@ -0,0 +1,12 @@
+// Apr 14, 2023
+// Ben Roberts
+
+import SwiftUI
+
+final class AddActivityButtomVM: ObservableObject {
+    let index: Int
+    
+    init(index: Int) {
+        self.index = index
+    }
+}
