1fbd6568f3566fc2b99cf389f42642e51de7f53f
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-04-14 00:44:05 -0400

    Added styling to the add activity button

---
+++ Shuffle Trip/Custom Views/Main Screen/Drawers/Selected Trip/Body Content/Add Activity Button/AddActivityButtonV.swift
@@ -0,0 +1,32 @@
+// Apr 14, 2023
+// Ben Roberts
+
+import SwiftUI
+
+struct AddActivityButtonV: View {
+    @StateObject var controller: AddActivityButtomVM
+    init(index: Int) {
+        self._controller = StateObject(wrappedValue: AddActivityButtomVM(index: index))
+    }
+    
+    var body: some View {
+        Button(action: {
+            
+        }, label: {
+            HStack {
+                Image(systemName: "plus.circle.fill")
+                    .symbolRenderingMode(.multicolor)
+                Text("Add Activity - \(controller.index)")
+            }
+            .padding(5)
+            .background(.tertiary)
+            .cornerRadius(10)
+        })
+    }
+}
+
+struct AddActivityButtonV_Previews: PreviewProvider {
+    static var previews: some View {
+        AddActivityButtonV(index: 0)
+    }
+}
