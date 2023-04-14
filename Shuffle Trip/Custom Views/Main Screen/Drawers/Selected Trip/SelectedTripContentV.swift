a47f02d42952a0cff7f5061a3e5e702e0bbe51f6
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-04-14 15:03:34 -0400

    Only show disclosure group if activity is generated

--- Shuffle Trip/Custom Views/Main Screen/Drawers/Selected Trip/SelectedTripContentV.swift
+++ Shuffle Trip/Custom Views/Main Screen/Drawers/Selected Trip/SelectedTripContentV.swift
@@ -19,7 +19,17 @@ struct SelectedTripContentV: View {
                     .padding(.vertical)
 
                 ForEach(controller.tripLocations.getSelectedTrip()?.activityLocations.indices ?? 1..<2, id: \.self) { index in
-                    ActivityPaneV(activity: controller.tripLocations.getSelectedTrip()?.activityLocations[index] ?? Activity(), index: index + 1)
+                    if controller.tripLocations.getSelectedTrip()?.activityLocations[index].businesses != nil {
+                        ActivityPaneV(activity: controller.tripLocations.getSelectedTrip()?.activityLocations[index] ?? Activity(), index: index + 1)
+                            .padding(.horizontal, 3)
+                            .background(BlurView(style: .systemUltraThinMaterial, opacity: 0))
+                            .cornerRadius(7)
+                            .padding(.horizontal, 3)
+                            .shadow(color: .primary.opacity(0.1), radius: 20)
+                    }
+                    else {
+                        Text("Planned")
+                    }
                     AddActivityButtonV(tripLocations: controller.tripLocations, index: index + 1)
                         .padding(.vertical)
                 }
