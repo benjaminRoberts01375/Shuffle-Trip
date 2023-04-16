18c3475775ebb28bb2d9c627a9d5eb362250f32f
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-04-15 22:55:13 -0400

    Docs

--- Shuffle Trip/Custom Views/Main Screen/Drawers/Home/HomeVM.swift
+++ Shuffle Trip/Custom Views/Main Screen/Drawers/Home/HomeVM.swift
@@ -5,7 +5,9 @@ import MapKit
 import SwiftUI
 
 class HomeVM: ObservableObject {
+    /// All user locations for the current user
     @Published var tripLocations: TripLocations
+    /// Current position of the map
     var region: RegionDetails
     
     init(tripLocations: TripLocations, region: RegionDetails) {
@@ -15,12 +17,17 @@ class HomeVM: ObservableObject {
     
     /// Handles logic for the add/remove trip button that is on the bottom drawer
     public func TripButton() {
-        guard let trip = tripLocations.tripLocations.first(where: { $0.isSelected }) else { tripLocations.AddTrip(trip: TripLocation(coordinate: region.region.center)); return }
-        if trip.coordinate == region.region.center {
-            tripLocations.RemoveTrip(trip: trip)
-        }
+        guard let trip = tripLocations.getSelectedTrip()                                    // Ensure that there is a selected trip
         else {
-            tripLocations.AddTrip(trip: TripLocation(coordinate: region.region.center))
+            tripLocations.AddTrip(trip: TripLocation(coordinate: region.region.center));    // Otherwise create a new trip at the current location...
+            return                                                                              // ...and return
+        }
+        
+        if trip.coordinate == region.region.center {                                        // If we're currently on top of a trip's center...
+            tripLocations.RemoveTrip(trip: trip)                                                // Remove that trip
+        }
+        else {                                                                              // Otherwise...
+            tripLocations.AddTrip(trip: TripLocation(coordinate: region.region.center))         // ...Create a trip at the current location
         }
     }
 }
