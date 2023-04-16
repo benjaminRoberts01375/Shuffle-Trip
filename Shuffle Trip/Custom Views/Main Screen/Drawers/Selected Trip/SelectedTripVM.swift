c6a1e12cc72881d6821200db9753c663dfb53625
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-04-15 22:58:59 -0400

    Docs

--- Shuffle Trip/Custom Views/Main Screen/Drawers/Selected Trip/SelectedTripVM.swift
+++ Shuffle Trip/Custom Views/Main Screen/Drawers/Selected Trip/SelectedTripVM.swift
@@ -4,10 +4,15 @@
 import SwiftUI
 
 @MainActor class SelectedTripVM: ObservableObject {
+    /// Current trip locations for the user
     @ObservedObject var tripLocations: TripLocations
+    /// Keeps track of the current editing status
     @ObservedObject var editingTracker: EditingTrackerM
+    /// Determines the current phase of the drawer based on the selected trips
     @Published var displayPhase: DisplayPhase
+    /// Helps prevent having to unwrap over and over
     @Published var selectedTrip: TripLocation!
+    /// Prevent the user from closing the drawer via the drawer
     @Published var disableCloseButton: Bool
     
     enum DisplayPhase {
@@ -30,15 +35,15 @@ import SwiftUI
     
     /// Determines how a given trip is configured, and sets the display phase
     internal func setDisplayPhase() {
-        setSelectedTrip()
-        guard selectedTrip != nil else { return }
-        switch selectedTrip.status {
-        case .successful, .unknown:
-            displayPhase = .info
-        case .error:
-            displayPhase = .error
-        case .generating:
-            displayPhase = .loading
+        setSelectedTrip()                           // Get the current trip
+        guard selectedTrip != nil else { return }   // If there's no current trip, return
+        switch selectedTrip.status {                // Use the status of the trip to determine what to display
+        case .successful, .unknown:                     // If good or unknown...
+            displayPhase = .info                            // Show general information
+        case .error:                                    // If we errored...
+            displayPhase = .error                           // Then display a generic error
+        case .generating:                               // If we're generating
+            displayPhase = .loading                         // Display a loading message
         }
     }
 }
