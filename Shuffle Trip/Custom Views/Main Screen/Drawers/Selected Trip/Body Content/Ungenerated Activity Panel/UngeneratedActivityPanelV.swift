97a26746ed354cf1f9ed5b9c1aa490dc8a4a9a5a
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-04-16 00:45:28 -0400

    Created UngeneratedActivityPanel objects

---
+++ Shuffle Trip/Custom Views/Main Screen/Drawers/Selected Trip/Body Content/Ungenerated Activity Panel/UngeneratedActivityPanelV.swift
@@ -0,0 +1,16 @@
+// Apr 16, 2023
+// Ben Roberts
+
+import SwiftUI
+
+struct UngeneratedActivityPanelV: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UngeneratedActivityPanelV_Previews: PreviewProvider {
+    static var previews: some View {
+        UngeneratedActivityPanelV()
+    }
+}
---
+++ Shuffle Trip/Custom Views/Main Screen/Drawers/Selected Trip/Body Content/Ungenerated Activity Panel/UngeneratedActivityPanelVM.swift
@@ -0,0 +1,8 @@
+// Apr 16, 2023
+// Ben Roberts
+
+import SwiftUI
+
+final class UngeneratedActivityPaneVM: ObservableObject {
+    
+}
