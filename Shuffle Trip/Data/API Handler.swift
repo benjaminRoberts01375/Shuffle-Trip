79f5a6e8267451a216ed178168572ba5988c9ca1
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-04-15 23:23:50 -0400

    Docs

--- Shuffle Trip/Data/API Handler.swift
+++ Shuffle Trip/Data/API Handler.swift
@@ -11,12 +11,19 @@ struct APIHandler {
         case friendDetauls = "getFriendsTrips"
     }
     
+    /// Errors available to throw
     enum Errors: String, Error {
+        /// Unable to decode data from the server.
         case decodeError = "Unable to decode data from the server."
+        /// Got no data back from the server.
         case emptyResponse = "Got no data back from the server."
+        /// Unable to encode data into a request.
         case encodeError = "Unable to encode data into a request."
+        /// Unable to connect to the server.
         case hostConnectionError = "Unable to connect to the server."
+        /// Unknown error.
         case sendPOSTError = "Unknown error."
+        /// Not able to generate server URL.
         case urlError = "Not able to generate server URL."
     }
     
@@ -28,37 +35,36 @@ struct APIHandler {
     /// - Returns: The decoded data
     /// - Throws: An error from the errors enum that is a part of this struct
     static func request<D: Decodable, E: Encodable>(url addition: APIUrl, dataToSend: E, decodeType: D.Type) async throws -> D {
-        guard let requestUrl = URL(string: baseURL + addition.rawValue) else { throw Errors.urlError }
-        guard let jsonData = try? JSONEncoder().encode(dataToSend) else { throw Errors.encodeError }
+        guard let requestUrl = URL(string: baseURL + addition.rawValue) else { throw Errors.urlError }  // Build URL to send POST request to
+        guard let jsonData = try? JSONEncoder().encode(dataToSend) else { throw Errors.encodeError }    // Encode JSON
         
-        var request = URLRequest(url: requestUrl)
-        request.httpMethod = "POST"
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.httpBody = jsonData
+        var request = URLRequest(url: requestUrl)                                                       // Create request
+        request.httpMethod = "POST"                                                                     // Set request to POST
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")                        // Add request header
+        request.httpBody = jsonData                                                                     // Add JSON to request
         
-        let (data, response) = try await URLSession.shared.data(for: request)
+        let (data, response) = try await URLSession.shared.data(for: request)                           // Send JSON and wait for response
         
         print(String(decoding: data, as: UTF8.self))
         
-        guard let httpResponse = response as? HTTPURLResponse else {
+        guard let httpResponse = response as? HTTPURLResponse else {                                    // Check against HTTP error
             print("Host connection error")
             throw Errors.hostConnectionError
         }
-        guard (200...299).contains(httpResponse.statusCode) else {
+        guard (200...299).contains(httpResponse.statusCode) else {                                      // Check status code
             print("POST error")
             throw Errors.sendPOSTError
         }
-        guard !data.isEmpty else {
+        guard !data.isEmpty else {                                                                      // Check data response
             print("Empty response")
             throw Errors.emptyResponse
         }
         
-        // Decode the response data-
-        guard let decodedData = try? JSONDecoder().decode(decodeType, from: data) else {
+        guard let decodedData = try? JSONDecoder().decode(decodeType, from: data) else {                // All is good, decode data if possible
             print("Decode error")
             throw Errors.decodeError
         }
         
-        return decodedData
+        return decodedData                                                                              // Return decoded data
     }
 }
