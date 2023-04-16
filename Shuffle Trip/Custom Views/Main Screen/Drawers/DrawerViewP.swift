4fd7c305ad332c21b124d04f8dab530783b6a50f
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-04-15 22:52:02 -0400

    Docs

--- Shuffle Trip/Custom Views/Main Screen/Drawers/DrawerViewP.swift
+++ Shuffle Trip/Custom Views/Main Screen/Drawers/DrawerViewP.swift
@@ -7,6 +7,8 @@ protocol DrawerView {
     associatedtype Header: View
     associatedtype Body: View
     
+    /// What to show in the header of a card
     var header: Header { get }
+    /// What to show in the body of a card
     var body: Body { get }
 }
