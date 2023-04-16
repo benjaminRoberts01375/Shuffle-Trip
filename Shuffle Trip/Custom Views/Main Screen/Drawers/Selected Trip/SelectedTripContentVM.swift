0f59213f8d5c8cd578cd359262e3efcd0f9357b5
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-04-15 23:01:09 -0400

    Docs

--- Shuffle Trip/Custom Views/Main Screen/Drawers/Selected Trip/SelectedTripContentVM.swift
+++ Shuffle Trip/Custom Views/Main Screen/Drawers/Selected Trip/SelectedTripContentVM.swift
@@ -4,8 +4,11 @@
 import SwiftUI
 
 final class SelectedTripContentVM: ObservableObject {
+    /// All available trip locations for the user
     @ObservedObject var tripLocations: TripLocations
+    /// Keeps track of the current editing status for the user
     @ObservedObject var editingTracker: EditingTrackerM
+    /// Keeps track of when to show the available settings for the trip
     @Published var showSettings: Bool
     
     init(tripLocations: TripLocations, editingTracker: EditingTrackerM) {
