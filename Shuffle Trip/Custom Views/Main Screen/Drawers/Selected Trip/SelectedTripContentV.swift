74f2c63e89cc208e3e12334961bb6a455a60687a
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-04-21 16:06:42 -0400

    Enable searching for activities

--- Shuffle Trip/Custom Views/Main Screen/Drawers/Selected Trip/SelectedTripContentV.swift
+++ Shuffle Trip/Custom Views/Main Screen/Drawers/Selected Trip/SelectedTripContentV.swift
@@ -18,51 +18,62 @@ struct SelectedTripContentV: View {
                 Color.clear
                     .frame(width: 0, height: 10)
                 if controller.editingTracker.isEditingTrip {
-                    TripConfiguratorV(tripLocations: controller.tripLocations)
-                        .padding(3)
-                        .background(BlurView(style: .systemThinMaterial, opacity: 0))
-                        .cornerRadius(7)
-                        .shadow(color: .secondary.opacity(0.3), radius: 2)
-                        .padding(.horizontal, 4)
-                    
-                    AddActivityButtonV(tripLocations: controller.tripLocations, activity: nil)
-                        .shadow(color: .secondary.opacity(0.3), radius: 2)
-                        .padding(6)
-                        .padding(.horizontal, 4)
+                    SearchBar(text: $controller.searchTracker.searchText, placeholder: "Find an activity...")
                 }
-                
-                ForEach(controller.tripLocations.getSelectedTrip()?.activityLocations ?? [], id: \.id) { activity in
-                    if activity.businesses != nil {
-                        ActivityPaneV(activity: activity, tripLocations: controller.tripLocations)
-                            .padding(6)
-                            .background(BlurView(style: .systemThinMaterial, opacity: 0))
-                            .cornerRadius(7)
-                            .shadow(color: .secondary.opacity(0.3), radius: 2)
-                            .padding(.horizontal, 4)
-                    }
-                    else {
-                        UngeneratedActivityPanelV(tripLocations: controller.tripLocations, activity: activity, editingTracker: controller.editingTracker)
-                            .padding(6)
-                            .background(BlurView(style: .systemThinMaterial, opacity: 0))
-                            .cornerRadius(7)
-                            .shadow(color: .secondary.opacity(0.3), radius: 2)
-                            .padding(.horizontal, 4)
-                    }
-                    
+                if !controller.searchTracker.searchText.isEmpty {
+                    SearchV(searchTracker: controller.searchTracker)
+                }
+                else {
                     if controller.editingTracker.isEditingTrip {
-                        AddActivityButtonV(tripLocations: controller.tripLocations, activity: activity)
+                        TripConfiguratorV(tripLocations: controller.tripLocations)
+                            .padding(3)
+                            .background(BlurView(style: .systemThinMaterial, opacity: 0))
+                            .cornerRadius(7)
+                            .shadow(color: .secondary.opacity(0.3), radius: 2)
+                            .padding(.horizontal, 4)
+                        
+                        AddActivityButtonV(tripLocations: controller.tripLocations, activity: nil)
+                            .shadow(color: .secondary.opacity(0.3), radius: 2)
                             .padding(6)
+                            .padding(.horizontal, 4)
                     }
+                    
+                    ForEach(controller.tripLocations.getSelectedTrip()?.activityLocations ?? [], id: \.id) { activity in
+                        if activity.businesses != nil {
+                            ActivityPaneV(activity: activity, tripLocations: controller.tripLocations)
+                                .padding(6)
+                                .background(BlurView(style: .systemThinMaterial, opacity: 0))
+                                .cornerRadius(7)
+                                .shadow(color: .secondary.opacity(0.3), radius: 2)
+                                .padding(.horizontal, 4)
+                        }
+                        else {
+                            UngeneratedActivityPanelV(tripLocations: controller.tripLocations, activity: activity, editingTracker: controller.editingTracker)
+                                .padding(6)
+                                .background(BlurView(style: .systemThinMaterial, opacity: 0))
+                                .cornerRadius(7)
+                                .shadow(color: .secondary.opacity(0.3), radius: 2)
+                                .padding(.horizontal, 4)
+                        }
+                        
+                        if controller.editingTracker.isEditingTrip {
+                            AddActivityButtonV(tripLocations: controller.tripLocations, activity: activity)
+                                .padding(6)
+                        }
+                    }
+                    Color.clear
+                        .frame(width: 0, height: 10)
                 }
-                Color.clear
-                    .frame(width: 0, height: 10)
             }
             .onReceive(controller.tripLocations.objectWillChange) { _ in
                 controller.showSettings = true
                 self.controller.objectWillChange.send()
             }
             .onReceive(controller.editingTracker.objectWillChange) { _ in
-                controller.checkEditing()
+                controller.checkState()
+            }
+            .onReceive(controller.searchTracker.$searchText) { _ in
+                controller.checkState()
             }
         }
         else {
--- Shuffle Trip/Custom Views/Main Screen/Drawers/Selected Trip/SelectedTripContentVM.swift
+++ Shuffle Trip/Custom Views/Main Screen/Drawers/Selected Trip/SelectedTripContentVM.swift
@@ -10,15 +10,18 @@ final class SelectedTripContentVM: ObservableObject {
     @ObservedObject var editingTracker: EditingTrackerM
     /// Keeps track of when to show the available settings for the trip
     @Published var showSettings: Bool
+    /// Tracking details about search in regards to location
+    @ObservedObject var searchTracker: LocationSearchTrackerM
     
     init(tripLocations: TripLocations, editingTracker: EditingTrackerM) {
         self.tripLocations = tripLocations
         self.editingTracker = editingTracker
         self.showSettings = false
+        self.searchTracker = LocationSearchTrackerM()
     }
     
     // Check editing status
-    internal func checkEditing() {
+    internal func checkState() {
         self.objectWillChange.send()
     }
 }
