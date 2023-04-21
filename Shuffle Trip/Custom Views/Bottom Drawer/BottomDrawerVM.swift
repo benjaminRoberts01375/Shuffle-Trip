5c17289fdbe9add60a208c6ca59b33dcf490bb8a
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-04-21 13:57:29 -0400

    Whitespace linter warning

--- Shuffle Trip/Custom Views/Bottom Drawer/BottomDrawerVM.swift
+++ Shuffle Trip/Custom Views/Bottom Drawer/BottomDrawerVM.swift
@@ -216,7 +216,6 @@ import SwiftUI
         previousDrag = CGSize(width: 0, height: 0)                                          // Reset dragging
     }
     
-    
     /// To implement tap to snap from bottom to middle, middle to top and top to bottom
     /// - Parameter animation: snap animations
     public func TapSnapToPoint(animation: Animation = Animation.interactiveSpring(response: 0.2, dampingFraction: 1, blendDuration: 0.2)) {
