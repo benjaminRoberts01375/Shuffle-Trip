b9ea27e3740c39ab7c6df1f13dc1fe6f3b590c28
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-04-21 03:39:16 -0400

    Allow for searching for locations with natural language

--- Shuffle Trip/Custom Views/Main Screen/Drawers/Home/LocationSearchTrackerM.swift
+++ Shuffle Trip/Custom Views/Main Screen/Drawers/Home/LocationSearchTrackerM.swift
@@ -15,6 +15,31 @@ final class LocationSearchTrackerM: ObservableObject {
     init() {
         self.searchText = ""
     }
+    
+    /// Searches for locations based on natural language
+    internal func searchLocation() {
+        print("Searching \(searchText)")
+        let request = MKLocalSearch.Request()
+        request.pointOfInterestFilter = MapDetails.defaultFilter
+        request.naturalLanguageQuery = searchText
+        
+        let search = MKLocalSearch(request: request)
+        search.start { (response, error) in
+            if error != nil {
+                print("Search error")
+                return
+            }
+            guard let response = response
+            else {
+                print("Response error")
+                return
+            }
+            
+            if response.mapItems.isEmpty {
+                print("No items")
+                return
+            }
+            
             print("Responses: \(response.mapItems)")
             
         }
