56f5103cf120f449614ddfb8042e892845a59020
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-04-22 04:54:04 -0400

    Created struct for creating QR codes

--- Shuffle Trip/Data/QRM.swift
+++ Shuffle Trip/Data/QRM.swift
@@ -10,4 +10,14 @@ struct QRM {
     static let context = CIContext()
     static let filter = CIFilter.qrCodeGenerator()
     
+    static func generateQRCode(data: String) -> UIImage {
+        filter.message = Data(data.utf8)
+        
+        if let outputImage = filter.outputImage {
+            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgimg)
+            }
+        }
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
+    }
 }
