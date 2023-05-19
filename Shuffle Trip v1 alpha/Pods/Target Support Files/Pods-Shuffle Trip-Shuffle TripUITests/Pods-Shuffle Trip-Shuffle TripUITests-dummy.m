77cc82651591a2f9d1560a8ec8a8fedf7231affe
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-05-19 16:29:27 -0400

    Moved Shuffle Trip project into v1 alpha

---
+++ Shuffle Trip v1 alpha/Pods/Target Support Files/Pods-Shuffle Trip-Shuffle TripUITests/Pods-Shuffle Trip-Shuffle TripUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Shuffle_Trip_Shuffle_TripUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Shuffle_Trip_Shuffle_TripUITests
+@end
---
+++ Shuffle Trip v1 alpha/Pods/Target Support Files/Pods-Shuffle TripTests/Pods-Shuffle TripTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Shuffle_TripTests : NSObject
+@end
+@implementation PodsDummy_Pods_Shuffle_TripTests
+@end
