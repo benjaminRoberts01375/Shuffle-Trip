7ed01d31969734478d47ab595abf22adcfc3e74f
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2023-05-19 16:41:23 -0400

    Created new Shuffle Trip project
    Now with macOS support

---
+++ Shuffle Trip/ContentView.swift
@@ -0,0 +1,25 @@
+// Created on 5/19/23 by Ben Roberts
+// Created for Shuffle Trip
+//
+// Swift 5.0
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shuffle Trip/Shuffle_TripApp.swift
@@ -0,0 +1,16 @@
+// Created on 5/19/23 by Ben Roberts
+// Created for Shuffle Trip
+//
+// Swift 5.0
+
+
+import SwiftUI
+
+@main
+struct Shuffle_TripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
